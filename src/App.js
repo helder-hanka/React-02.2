7b558c60e7617fb67f18a4efac65e4641652472d
helder-hanka <hfernandes238@gmail.com>
2020-04-03 11:43:43 +0200

    Add ContactList.js;d modified Add.js and Contact

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import Contact from './components/Contatc'
+import ContactList from './components/ContactList';
 
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <Contact name="Jimmie Stephans" avatar="https://randomuser.me/api/portraits/men/74.jpg" online />
-        <Contact name="Thomas Cooper" avatar="https://randomuser.me/api/portraits/men/67.jpg" />
-        <Contact name="Delores Sims" avatar="https://randomuser.me/api/portraits/women/24.jpg" online />
+        <ContactList />
       </div>
     );
   }
 }
 
 export default App;
+
+// import React, { Component } from 'react';
+// import Contact from './components/Contatc'
+
+
+// class App extends Component {
+//   render () {
+//     return (
+//       <div className="App">
+//         <Contact name="Jimmie Stephans" avatar="https://randomuser.me/api/portraits/men/74.jpg" online />
+//         <Contact name="Thomas Cooper" avatar="https://randomuser.me/api/portraits/men/67.jpg" />
+//         <Contact name="Delores Sims" avatar="https://randomuser.me/api/portraits/women/24.jpg" online />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
